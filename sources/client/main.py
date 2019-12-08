b6a08f8258d1b26e0f89d2dd329cc3947d60fd21
vanhehe <iakarpov@miem.hse.ru>
2019-12-08 13:02:31 +0300

    basic gui world

--- sources/client/main.py
+++ sources/client/main.py
@@ -1,5 +1,28 @@
-def main():
-    pass
+from tkinter import *
 
-if __name__ == "__main__":
-    main()
+# Создаем окно
+root = Tk()
+# Устанавливаем название окна
+root.title("RAINBOW hit")
+
+GRID_SIZE = 10
+SQUARE_SIZE = 50
+# ширина экрана
+WIDTH = GRID_SIZE * SQUARE_SIZE
+# высота экрана
+HEIGHT = GRID_SIZE *SQUARE_SIZE
+# Размер сегмента танка
+SEG_SIZE = 30
+# Переменная отвечающая за состояние игры(взять у ребят с серверной части)
+IN_GAME = True
+
+# заливаем все зеленым цветом
+c = Canvas(root, width=WIDTH, height=HEIGHT, bg="#105410")
+#делаем сетку
+for i in range(GRID_SIZE):
+    for j in range(GRID_SIZE):
+        c.create_rectangle(i * SQUARE_SIZE, j * SQUARE_SIZE,
+                           i * SQUARE_SIZE + SQUARE_SIZE,
+                           j * SQUARE_SIZE + SQUARE_SIZE)
+c.grid(column=0, row=0)
+c.mainloop()

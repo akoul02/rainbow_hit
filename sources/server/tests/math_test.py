8fd62202093b7af024786dfd67f31e43de4846a4
Kirill Solovyev <k.solovyev@MacBook-Pro-Kirill.local>
2019-12-12 03:49:02 +0300

    udated tests

--- sources/server/tests/math_test.py
+++ sources/server/tests/math_test.py
@@ -12,21 +12,35 @@ from engine.gameobjects.wall import Wall
 
 class MathTest(unittest.TestCase):
 
-    def test_max_hp_over(self):
+    def test_max_hp_over1(self):
         world = World()
-        wall = Wall(Point(0, 0), world, 95, 100, True)
+        wall = Wall(Point(6, 3), world, 95, 100, True)
         self.assertEqual(wall.increase_health(10), 100)
 
-    def test_min_hp_lower(self):
+    def test_max_hp_over2(self):
         world = World()
-        wall = Wall(Point(0, 0), world, 35, 100, True)
+        wall = Wall(Point(2, 1), world, 30, 100, True)
+        self.assertEqual(wall.increase_health(100), 100)
+
+    def test_min_hp_lower1(self):
+        world = World()
+        wall = Wall(Point(5, 9), world, 35, 100, True)
         self.assertEqual(wall.reduce_health(45), 0)
 
-    def test_hp_average(self):
+    def test_min_hp_lower2(self):
+        world = World()
+        wall = Wall(Point(12, 0), world, 70, 100, True)
+        self.assertEqual(wall.reduce_health(100), 0)
+
+    def test_hp_average1(self):
         world = World()
         wall = Wall(Point(4, 4), world, 100, 100, True)
         self.assertEqual(wall.reduce_health(50), 50)
 
+    def test_hp_average2(self):
+        world = World()
+        wall = Wall(Point(8, 2), world, 100, 100, True)
+        self.assertEqual(wall.reduce_health(70), 30)
 
 
 if __name__ == "__main__":

d674d510c1af61572fd8e22dc9ddfd160dfe141e
Kirill Solovyev <k.solovyev@MacBook-Pro-Kirill.local>
2019-12-19 03:45:33 +0300

    bot algorithm

--- sources/server/game/bots_code/code.py
+++ sources/server/game/bots_code/code.py
@@ -112,3 +112,43 @@ def run_user2(bot: Bot):
         if world[coord.x][coord.y]:
             return True
         return False
+
+    def check(world: List[List[int]], dst: Point, current: Point, steps: deque) -> bool:
+        if current.x + dst.x >= 0 and current.x + dst.x < 16 and current.y + dst.y >= 0 and current.y + dst.y < 16:
+            if not at_position(world, current + dst):
+                if not (current + dst in steps):
+                    return True
+        return False
+    
+    clean_directions = list(Direction)
+
+    while True:
+        for obj in bot.scan():
+            if isinstance(obj, Wall):
+                objects[obj.coord.x][obj.coord.y] = 1
+            else:
+                # enemy
+                objects[obj.coord.x][obj.coord.y] = 2
+
+        for obj in bot.scan():
+            if isinstance(obj, UserBot):
+                bot.shoot(obj)
+                break
+
+        directions = clean_directions.copy()
+        random.shuffle(directions)
+        
+        for idx in range(len(directions)):
+            if check(objects, directions[idx].value, bot.coord, steps):
+                steps.append(bot.coord.copy())
+                step_directions.append(directions[idx])
+                last_dir = directions[idx]
+                bot.step(directions[idx])
+                break
+            else:
+                continue
+        
+        if idx == len(directions) - 1:
+            steps.append(bot.coord.copy())
+            direction = ~step_directions.pop()
+            bot.step(direction)

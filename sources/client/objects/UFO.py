a560ae1d54d3a9704aac5f6b06bb636c4a165068
vanhehe <iakarpov@miem.hse.ru>
2019-12-22 16:11:24 +0300

    some more documentation

--- sources/client/objects/UFO.py
+++ sources/client/objects/UFO.py
@@ -6,26 +6,57 @@ from client.const_client import *
 
 class Ufo:
     """Class of UFO. Create it and process actions
+
+    Attributes
+    ----------
+    x : int
+    y : int
+    canvas : Canvas
+        game filled
+
+    sprite : Canvas
+        photos of UFOs on canvas
+
+    name: str
+        unique identifier of UFOs
+
     """
     def __init__(self, x, y, canvas, way, name):
         """Initialising values and creation UFO
         """
         self.x = x
         self.y = y
-        self.pilImage = Image.open(way)
-        self.image = ImageTk.PhotoImage(self.pilImage)
         self.canvas = canvas
+        self.image = ImageTk.PhotoImage(Image.open(way))
         self.sprite = self.canvas.create_image(self.x, self.y, image=self.image)
         self.name = name
 
     def move(self, x, y, Event=None):
         """Moving UFO to adjacent cell
+
+        Parameters
+        ----------
+        x : int
+        y : int
+
+        Returns
+        -------
+        None
         """
         self.canvas.move(self.sprite, (48 + x * 32) - self.x, (32 * 18 - (48 + y * 32)) - self.y)
         self.x, self.y = self.canvas.coords(self.sprite)
 
     def laser(self, x, y, Event=None):
         """Laser from UFO to given coordinates
+
+        Parameters
+        ----------
+        x : int
+        y : int
+
+        Returns
+        -------
+        None
         """
         x1, y1 = self.canvas.coords(self.sprite)
         canvas_id = self.canvas.create_line(x1, y1, 48 + x * 32, 32 * 18 - (48 + y * 32), fill='red', width='3')
@@ -33,5 +64,9 @@ class Ufo:
 
     def deleter(self, Event=None):
         """Delete UFO if it dies
+
+        Returns
+        -------
+        None
         """
         self.canvas.delete(self.sprite)
--- sources/client/objects/cloud.py
+++ sources/client/objects/cloud.py
@@ -5,16 +5,27 @@ from client.const_client import *
 
 class Cloud:
     """Creating and deleting clouds
+
+    Attributes
+    ----------
+    x : int
+    y : int
+    canvas : Canvas
+        game filled
+
+    sprite : Canvas
+        photos of clouds on canvas
     """
+
     def __init__(self, x, y, canvas, way):
         """Create cloud on x, y coordinates
         """
         self.x = x
         self.y = y
-        self.pilImage = Image.open(way)
-        self.image = ImageTk.PhotoImage(self.pilImage)
         self.canvas = canvas
+        self.image = ImageTk.PhotoImage(Image.open(way))
         self.sprite = self.canvas.create_image(self.x, self.y, image=self.image)
+
     def deleter(self, Event=None):
         """Delete Cloud if it dies
         """
